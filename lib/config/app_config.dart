3702089ba9f16afefc8de96763952f8b5103e5c5
azeunkn0wn <azeunkn0wn@gmail.com>
2023-06-19 18:43:50 +0800

    app configuration for each flavor

---
+++ lib/config/app_config.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_flavor_concept/models/app_config_model.dart';
+import 'package:flutter_flavor_concept/utils/theme_data.dart';
+
+final AppConfig meetYodaAppConfig = AppConfig(
+  title: 'MeetYoda',
+  description: 'MeetYoda App',
+  themeData: meetYodaThemeLight,
+  envPath: 'env/meetyoda/',
+  logo: 'assets/images/meetyoda-logo.png',
+);
+
+final AppConfig myvcmoAppConfig = AppConfig(
+  title: 'myvcmo',
+  description: 'myvcmo App',
+  themeData: myvcmoThemeLight,
+  envPath: 'env/myvcmo/',
+  logo: 'assets/images/crm.jpg',
+);

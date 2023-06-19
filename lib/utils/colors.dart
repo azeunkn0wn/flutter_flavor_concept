064ebab55bb869848a56d9a3458dea16fdbcb630
azeunkn0wn <azeunkn0wn@gmail.com>
2023-06-19 18:37:56 +0800

    files from project with some modification

---
+++ lib/utils/colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color.fromRGBO(141, 13, 49, 1);
+const secondaryTextColor = Color.fromRGBO(51, 51, 51, 1);
+const accentColor = Color.fromRGBO(141, 13, 49, 1);
+const scaffoldColor = Color(0xffe5e5e5);
+const onHoldButtonColor = Color(0xffffc109);
+const completeButtonColor = Color.fromRGBO(40, 167, 69, 1);
+const cancelButtonColor = Color(0xffdc3545);
+const loginFieldsBackgroundColor = Color.fromRGBO(255, 255, 255, 0.1);
+const loginFieldsBorderColor = Color.fromRGBO(255, 255, 255, 0.3);
+const loginFieldsLabelColor = Color.fromRGBO(255, 255, 255, 0.6);
+const loginButtonColor = Color.fromRGBO(255, 255, 255, 1);
+const loginButtonFontColor = Color.fromRGBO(169, 19, 52, 1);
+// myvcmo color scheme
+const primaryColor2 = Color.fromRGBO(13, 141, 49, 1);
+const secondaryTextColor2 = Color.fromRGBO(51, 51, 51, 1);
+const accentColor2 = Color.fromRGBO(13, 141, 49, 1);
+const scaffoldColor2 = Color(0xffe5e5e5);
+const onHoldButtonColor2 = Color(0xffffc109);
+const completeButtonColor2 = Color.fromRGBO(40, 167, 69, 1);
+const cancelButtonColor2 = Color(0xffdc3545);
+const loginFieldsBackgroundColor2 = Color.fromRGBO(255, 255, 255, 0.1);
+const loginFieldsBorderColor2 = Color.fromRGBO(255, 255, 255, 0.3);
+const loginFieldsLabelColor2 = Color.fromRGBO(255, 255, 255, 0.6);
+const loginButtonColor2 = Color.fromRGBO(255, 255, 255, 1);
+const loginButtonFontColor2 = Color.fromRGBO(169, 19, 52, 1);
---
+++ lib/utils/endpoints.dart
@@ -0,0 +1,5 @@
+import '../config/environments.dart';
+
+class Endpoints {
+  static String servertUrl = "${Environment.baseURL}/api/v2";
+}
